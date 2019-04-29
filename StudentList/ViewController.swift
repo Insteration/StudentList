70332cc1ed20c07f6f59a4e87d0deb84efeea52e
Art Karma <inste@icloud.com>
2019-04-29 21:19:44 +0300

    Third commit

--- StudentList/ViewController.swift
+++ StudentList/ViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var studentListTableView: UITableView!
     // First commit
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        studentListTableView.dataSource = self
+
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "123"
+        return cell
+    }
+    
+    
+}
