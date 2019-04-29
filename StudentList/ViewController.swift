234a21752f98b61b5d9a288a8019b6ed969d322b
Art Karma <inste@icloud.com>
2019-04-29 21:05:40 +0300

    First commit

---
+++ StudentList/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StudentList
+//
+//  Created by Art Karma on 4/29/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // First commit
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
